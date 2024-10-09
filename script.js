43909489e3ff8735d19d65ab5d0d771c48cc5021
luisgustavo777 <hadockborracheiro2020@gmail.com>
2024-10-08 22:19:09 -0300

    criar função de converter de K para F/C

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ document.getElementById('fromCelsiusToKelvin').addEventListener('click', functio
     const temperature = parseFloat(document.getElementById('temperatureInput').value);
     if (!isNaN(temperature)) {
         const kelvin = (temperature + 273.15);
-        document.getElementById('result').innerText =  `${temperature} °C é igual a ${kelvin.toFixed(2)} °K`;
+        document.getElementById('result').innerText = `${temperature} °C é igual a ${kelvin.toFixed(2)} °K`;
     } else {
         document.getElementById('result').innerText = 'Por favor, insira um número válido'
     }
@@ -32,7 +32,27 @@ document.getElementById('fromFahrenheitToKelvin').addEventListener('click', func
     const temperature = parseFloat(document.getElementById('temperatureInput').value);
     if (!isNaN(temperature)) {
         const kelvin = (((temperature - 32) * 5) / 9) + 273.15;
-        document.getElementById('result').innerText =  `${temperature} °F é igual a ${kelvin.toFixed(2)} °K`;
+        document.getElementById('result').innerText = `${temperature} °F é igual a ${kelvin.toFixed(2)} °K`;
+    } else {
+        document.getElementById('result').innerText = 'Por favor, insira um número válido'
+    }
+});
+
+document.getElementById('fromKelvinToCelsius').addEventListener('click', function() {
+    const temperature = parseFloat(document.getElementById('temperatureInput').value);
+    if (!isNaN(temperature)) {
+        const celsius = (temperature - 273.15);
+        document.getElementById('result').innerText = `${temperature} °K é igual a ${celsius.toFixed(2)} °C`;
+    } else {
+        document.getElementById('result').innerText = 'Por favor, insira um número válido'
+    }
+});
+
+document.getElementById('fromKelvinToFahrenheit').addEventListener('click', function() {
+    const temperature = parseFloat(document.getElementById('temperatureInput').value);
+    if (!isNaN(temperature)) {
+        const fahrenheit = (((temperature - 273.15) * 9) / 5) + 32;
+        document.getElementById('result').innerText =  `${temperature} °K é igual a ${fahrenheit.toFixed(2)} °F`;
     } else {
         document.getElementById('result').innerText = 'Por favor, insira um número válido'
     }
